4dd78bf024fc63fd3a2a1f685b7e6588d4a6580e
Trevor Monteforte <monteforte.5@osu.edu>
2017-10-21 13:35:06 -0400

    Altereed Python file to accept strings and return results as a string

--- Sentiment.py
+++ Sentiment.py
@@ -4,17 +4,17 @@ from google.cloud.language import types
 
 # Instantiates a client
 client = language.LanguageServiceClient()
+def SentimentAnalysis(text):
+    # The text to analyze
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
 
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
+    sentimentScore = sentiment.score
 
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+    return sentimentScore
 
-
+print SentimentAnalysis("We are the best ever!")
