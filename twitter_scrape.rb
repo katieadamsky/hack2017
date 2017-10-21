5409b2eaf7c7860f7e9c7223430d7032093db241
Ethan Wolfe <ethanrw19@gmail.com>
2017-10-21 12:49:24 -0400

    Pulling 5 tweets example

--- twitter_scrape.rb
+++ twitter_scrape.rb
@@ -1,11 +1,12 @@
 require 'twitter'
 require 'nokogiri'
+require 'net/http'
 
 # Load api info from xml file
 file = File.read("twitter_key.xml")
 xml = Nokogiri::XML(file)
 config = {
-  consumer_key:    xml.xpath("//key").text,
+  consumer_key: xml.xpath("//key").text,
   consumer_secret: xml.xpath("//secret").text,
 }
 
@@ -13,3 +14,11 @@ config = {
 client = Twitter::REST::Client.new(config)
 
 # Query
+user = 'realDonaldTrump'
+options = {count: 5, include_rts: false}
+tweets = client.user_timeline(user, options)
+tweets.each do |tweet|
+  puts "Tweet"
+  puts tweet.full_text
+  puts ""
+end
